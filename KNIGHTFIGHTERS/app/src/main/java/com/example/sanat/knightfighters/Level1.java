1ffb52640b9951f8529178ea50975bd6d33d701d
KingRaj2015 <rajat_thomas@yahoo.com>
2015-08-23 23:56:32 +1000

    All Activities
    Includes activities for all levels

--- KNIGHTFIGHTERS/app/src/main/java/com/example/sanat/knightfighters/Level1.java
+++ KNIGHTFIGHTERS/app/src/main/java/com/example/sanat/knightfighters/Level1.java
@@ -7,44 +7,19 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.RelativeLayout;
 
-public class GameMap extends AppCompatActivity {
+public class Level1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game_map);
-
+        setContentView(R.layout.activity_level1);
     }
-    public void Level1(View v){
 
-        startActivity(new Intent(this, Level1background.class));
-        MediaPlayer mp = MediaPlayer.create(getApplicationContext(),R.raw.button);
-        mp.start();
-    }
-    public void Level2(View v){
-
-        startActivity(new Intent(this, Level2background.class));
-        MediaPlayer mp = MediaPlayer.create(getApplicationContext(),R.raw.button);
-        mp.start();
-    }
-    public void Level3(View v){
-
-        startActivity(new Intent(this, Level3background.class));
-        MediaPlayer mp = MediaPlayer.create(getApplicationContext(),R.raw.button);
-        mp.start();
-    }
-    public void Level4(View v){
-
-        startActivity(new Intent(this, Level4background.class));
-        MediaPlayer mp = MediaPlayer.create(getApplicationContext(),R.raw.button);
-        mp.start();
-    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_game_map, menu);
+        getMenuInflater().inflate(R.menu.menu_level1, menu);
         return true;
     }
 
--- KNIGHTFIGHTERS/app/src/main/java/com/example/sanat/knightfighters/Level4background.java
+++ KNIGHTFIGHTERS/app/src/main/java/com/example/sanat/knightfighters/Level4background.java
@@ -1,50 +1,48 @@
 package com.example.sanat.knightfighters;
 
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.RelativeLayout;
 
-public class GameMap extends AppCompatActivity {
+public class Level4background extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game_map);
+        setContentView(R.layout.activity_level4background);
+        RelativeLayout levelbg = (RelativeLayout)findViewById(R.id.levelbg);
+
+        levelbg.setOnTouchListener(
+                new RelativeLayout.OnTouchListener() {
+                    public boolean onTouch(View v, MotionEvent m) {
+
+                        handletouch(m);
+
+                        return true;
+
+                    }
+                }
+        );
+    }
+    public void handletouch(MotionEvent m){
+        if(m.getActionMasked() == MotionEvent.ACTION_DOWN){
+            startActivity(new Intent(this, Level4.class));
+            finish();
+
+        }
+
 
     }
-    public void Level1(View v){
 
-        startActivity(new Intent(this, Level1background.class));
-        MediaPlayer mp = MediaPlayer.create(getApplicationContext(),R.raw.button);
-        mp.start();
-    }
-    public void Level2(View v){
-
-        startActivity(new Intent(this, Level2background.class));
-        MediaPlayer mp = MediaPlayer.create(getApplicationContext(),R.raw.button);
-        mp.start();
-    }
-    public void Level3(View v){
-
-        startActivity(new Intent(this, Level3background.class));
-        MediaPlayer mp = MediaPlayer.create(getApplicationContext(),R.raw.button);
-        mp.start();
-    }
-    public void Level4(View v){
-
-        startActivity(new Intent(this, Level4background.class));
-        MediaPlayer mp = MediaPlayer.create(getApplicationContext(),R.raw.button);
-        mp.start();
-    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_game_map, menu);
+        getMenuInflater().inflate(R.menu.menu_level4background, menu);
         return true;
     }
 
