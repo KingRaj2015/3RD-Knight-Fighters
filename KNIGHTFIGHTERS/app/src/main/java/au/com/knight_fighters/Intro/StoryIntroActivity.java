822e438dce6d063abef074c4a391dfd500f3d457
KingRaj2015 <rajat_thomas@yahoo.com>
2015-09-21 20:45:59 +1000

    Intro Video
    Added the Introduction Video to the game.

--- KNIGHTFIGHTERS/app/src/main/java/au/com/knight_fighters/Intro/StoryIntroActivity.java
+++ KNIGHTFIGHTERS/app/src/main/java/au/com/knight_fighters/Intro/StoryIntroActivity.java
@@ -1,6 +1,8 @@
 package au.com.knight_fighters.Intro;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,6 +10,7 @@ import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.widget.RelativeLayout;
 import android.view.View;
+import android.widget.VideoView;
 
 import au.com.knight_fighters.Main.GameMap;
 import au.com.knight_fighters.R;
@@ -15,19 +18,35 @@ import au.com.knight_fighters.R;
 
 
 public class StoryIntroActivity extends AppCompatActivity {
+private VideoView myVideoView;
+    private int position;
 
-    @Override
+
+
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_back_ground);
+
+        myVideoView = (VideoView)findViewById(R.id.videoView);
+        myVideoView.setVideoPath("android.resource://"+ getPackageName()+"/"+R.raw.intro);
+        myVideoView.start();
+        myVideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                CallNextActivity();
+            }
+        });
 
         RelativeLayout gamebg = (RelativeLayout)findViewById(R.id.gamebg);
 
         gamebg.setOnTouchListener(
                 new RelativeLayout.OnTouchListener() {
                     public boolean onTouch(View v, MotionEvent m) {
-
                         handleTouch(m);
+
 
                         return true;
 
@@ -39,13 +58,35 @@ public class StoryIntroActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onPause() {
+        super.onPause();
+        position = myVideoView.getCurrentPosition();
+        myVideoView.pause();
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        myVideoView.seekTo(position);
+        myVideoView.start();
+
+    }
+
+
     public void handleTouch(MotionEvent m){
         if(m.getActionMasked() == MotionEvent.ACTION_DOWN){
-            startActivity(new Intent(this, GameMap.class));
-            finish();
+            CallNextActivity();
 
         }
 
+
+
+    }
+    public void CallNextActivity(){
+        startActivity(new Intent(this, GameMap.class));
+        finish();
 
     }
 
